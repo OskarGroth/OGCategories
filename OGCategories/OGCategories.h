e0d3aada8255eea66d1d175f90cacf952da57b51
Oskar Groth <oskar@cindori.org>
2017-04-09 14:33:14 +0200

    Initial Commit

---
+++ OGCategories/OGCategories.h
@@ -0,0 +1,19 @@
+//
+//  OGCategories.h
+//  OGCategories
+//
+//  Created by Oskar Groth on 2017-04-09.
+//  Copyright © 2017 Oskar Groth. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OGCategories.
+FOUNDATION_EXPORT double OGCategoriesVersionNumber;
+
+//! Project version string for OGCategories.
+FOUNDATION_EXPORT const unsigned char OGCategoriesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OGCategories/PublicHeader.h>
+
+
